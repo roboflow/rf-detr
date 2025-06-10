9d33b47cd682d06d5abbeae0b19c81d19c071893
MiXaiLL76 <mike.milos@yandex.ru>
2025-06-10 21:33:29 +0300

    extended updatehttps://github.com/MiXaiLL76/faster_coco_eval/blob/main/faster_coco_eval/core/faster_eval_api.py#L206

--- rfdetr/engine.py
+++ rfdetr/engine.py
@@ -319,7 +319,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, arg
         coco_evaluator.summarize()
     stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
     if coco_evaluator is not None:
-        results_json = coco_extended_metrics(coco_evaluator.coco_eval["bbox"])
+        results_json = coco_evaluator.coco_eval["bbox"].extended_metrics
         print("Results JSON:", results_json)
         stats["results_json"] = results_json
         if "bbox" in postprocessors.keys():
