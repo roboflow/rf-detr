26bb8655812ab76ecba4f0cdb2dd588e8c779e69
LeosCtrt <leoscoutrot@gmail.com>
2025-06-19 16:51:32 +0200

    Fix a forgotten default value

--- rfdetr/config.py
+++ rfdetr/config.py
@@ -31,7 +31,7 @@ class ModelConfig(BaseModel):
     group_detr: int = 13
     gradient_checkpointing: bool = False
     MoE: bool = False
-    MoE_params: List[int] = [16, 1]
+    MoE_params: List[int] = [32, 1]
 
 class RFDETRBaseConfig(ModelConfig):
     encoder: Literal["dinov2_windowed_small", "dinov2_windowed_base"] = "dinov2_windowed_small"
