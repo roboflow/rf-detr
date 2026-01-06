7c8b3ef7bfae0bab205644ce95eee126bb358606
Kai Christensen <matthew.kai.christensen@gmail.com>
2026-01-06 19:58:48 +0000

    copy-paste and patch pycocotools summarize method to not assume 100 maxDets

--- rfdetr/datasets/coco_eval.py
+++ rfdetr/datasets/coco_eval.py
@@ -38,11 +38,13 @@ class CocoEvaluator(object):
         assert isinstance(iou_types, (list, tuple))
         coco_gt = copy.deepcopy(coco_gt)
         self.coco_gt = coco_gt
+        self.max_dets = max_dets
 
         self.iou_types = iou_types
         self.coco_eval = {}
         for iou_type in iou_types:
             self.coco_eval[iou_type] = COCOeval(coco_gt, iouType=iou_type)
+            self.coco_eval[iou_type].params.maxDets = [1, 10, max_dets]
 
         self.img_ids = []
         self.eval_imgs = {k: [] for k in iou_types}
@@ -78,7 +80,7 @@ class CocoEvaluator(object):
     def summarize(self):
         for iou_type, coco_eval in self.coco_eval.items():
             print("IoU metric: {}".format(iou_type))
-            coco_eval.summarize()
+            patched_pycocotools_summarize(coco_eval)
 
     def prepare(self, predictions, iou_type):
         if iou_type == "bbox":
@@ -269,3 +271,83 @@ def evaluate(self):
 #################################################################
 # end of straight copy from pycocotools, just removing the prints
 #################################################################
+
+
+#################################################################
+# From pycocotools, but patched the first _summarize() call to
+# reference the last element of the maxDets list (like all the 
+# other calls to _summarize() do) instead of hardcoding maxDets to 100.
+#################################################################
+def patched_pycocotools_summarize(self):
+    '''
+    Compute and display summary metrics for evaluation results.
+    Note this functin can *only* be applied on the default parameter setting
+    '''
+    def _summarize(ap=1, iouThr=None, areaRng='all', maxDets=100):
+        p = self.params
+        iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
+        titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
+        typeStr = '(AP)' if ap==1 else '(AR)'
+        iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
+            if iouThr is None else '{:0.2f}'.format(iouThr)
+
+
+        aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
+        mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
+        if ap == 1:
+            # dimension of precision: [TxRxKxAxM]
+            s = self.eval['precision']
+            # IoU
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+            s = s[:,:,:,aind,mind]
+        else:
+            # dimension of recall: [TxKxAxM]
+            s = self.eval['recall']
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+            s = s[:,:,aind,mind]
+        if len(s[s>-1])==0:
+            mean_s = -1
+        else:
+            mean_s = np.mean(s[s>-1])
+        print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
+        return mean_s
+    def _summarizeDets():
+        stats = np.zeros((12,))
+        stats[0] = _summarize(1, maxDets=self.params.maxDets[2])
+        stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
+        stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
+        stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
+        stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
+        stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
+        stats[6] = _summarize(0, maxDets=self.params.maxDets[0])
+        stats[7] = _summarize(0, maxDets=self.params.maxDets[1])
+        stats[8] = _summarize(0, maxDets=self.params.maxDets[2])
+        stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
+        stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
+        stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+        return stats
+    def _summarizeKps():
+        stats = np.zeros((10,))
+        stats[0] = _summarize(1, maxDets=20)
+        stats[1] = _summarize(1, maxDets=20, iouThr=.5)
+        stats[2] = _summarize(1, maxDets=20, iouThr=.75)
+        stats[3] = _summarize(1, maxDets=20, areaRng='medium')
+        stats[4] = _summarize(1, maxDets=20, areaRng='large')
+        stats[5] = _summarize(0, maxDets=20)
+        stats[6] = _summarize(0, maxDets=20, iouThr=.5)
+        stats[7] = _summarize(0, maxDets=20, iouThr=.75)
+        stats[8] = _summarize(0, maxDets=20, areaRng='medium')
+        stats[9] = _summarize(0, maxDets=20, areaRng='large')
+        return stats
+    if not self.eval:
+        raise Exception('Please run accumulate() first')
+    iouType = self.params.iouType
+    if iouType == 'segm' or iouType == 'bbox':
+        summarize = _summarizeDets
+    elif iouType == 'keypoints':
+        summarize = _summarizeKps
+    self.stats = summarize()
