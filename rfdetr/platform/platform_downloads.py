eae293d68311e5cf7a08c060f8f1a865228afb19
Peter Robicheaux <peter@roboflow.com>
2026-01-14 10:41:03 -0800

    Fix

--- rfdetr/platform/platform_downloads.py
+++ rfdetr/platform/platform_downloads.py
@@ -1,5 +1,5 @@
 # THE FOLLOWING FILES IN THE ENTERPRISE FOLDER ARE COVERED BY THE PLATFORM MODEL LICENSE
 PLATFORM_MODELS = {
-    "rf-detr-xlarge.pth": "https://storage.googleapis.com/rfdetr/platform/rf-detr-xlarge.pth",
-    "rf-detr-xxlarge.pth": "https://storage.googleapis.com/rfdetr/platform/rf-detr-xxlarge.pth",
+    "rf-detr-xlarge.pth": "https://storage.googleapis.com/rfdetr/platform-licensed/rf-detr-xlarge.pth",
+    "rf-detr-xxlarge.pth": "https://storage.googleapis.com/rfdetr/platform-licensed/rf-detr-xxlarge.pth",
 }
