34519fac1e0d4606a2f7e2217f7807c3f816b6aa
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2026-01-14 22:17:35 +0100

    Apply suggestions from code review

--- rfdetr/__init__.py
+++ rfdetr/__init__.py
@@ -10,7 +10,7 @@ if os.environ.get("PYTORCH_ENABLE_MPS_FALLBACK") is None:
     os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 from rfdetr.platform.models import (
-    RFDETRXLargeConfig,
+    RFDETRXLarge,
     RFDETR2XLarge,
 )
 from rfdetr.detr import (
