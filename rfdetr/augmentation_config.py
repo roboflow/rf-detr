fd5f3b0fcb9eb14d9091d66849e6f5910537e81c
Panagiota Moraiti <panagiotamoraiti2@gmail.com>
2026-01-30 15:22:08 +0200

    Add license header to augmentations config

--- rfdetr/augmentation_config.py
+++ rfdetr/augmentation_config.py
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------------------
+# RF-DETR
+# Copyright (c) 2025 Roboflow. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+
 AUG_CONFIG = {
     "HorizontalFlip": {"p": 0.5},
     "VerticalFlip": {"p": 0.5},
