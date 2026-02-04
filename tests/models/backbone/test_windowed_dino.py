19148a66e2328c2cd95eaad1ddc075336d7d95a8
jirka <6035284+Borda@users.noreply.github.com>
2026-02-04 14:23:23 +0900

    Add copyright headers to test files in `models` module

--- tests/models/backbone/test_windowed_dino.py
+++ tests/models/backbone/test_windowed_dino.py
@@ -1,4 +1,10 @@
+# ------------------------------------------------------------------------
+# RF-DETR
+# Copyright (c) 2025 Roboflow. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
 import torch
+
 from rfdetr.models.backbone.dinov2_with_windowed_attn import (
     WindowedDinov2WithRegistersConfig,
     WindowedDinov2WithRegistersEmbeddings,
