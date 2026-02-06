3fd406f00f4220601bf6814a0fa9cd950cfdaa39
jirka <6035284+Borda@users.noreply.github.com>
2026-02-06 16:59:03 +0900

    epochs=10

--- tests/benchmarks/test_synthetic_convergence.py
+++ tests/benchmarks/test_synthetic_convergence.py
@@ -47,7 +47,7 @@ def test_synthetic_training_improves_performance(
         #do_random_resize_via_padding=False,
         square_resize_div_64=True,
         #print_freq=20,
-        epochs=7,
+        epochs=10,
     )
     train_config = {
         **vars(args),
